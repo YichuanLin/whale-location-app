859329429925d690bb688f1b080ea38b0ea759fd
Yichuan Lin Lee <yichuan.lin.sngular@xbyorange.com>
2019-11-13 16:44:46 +0100

    Define application routes

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import Home from "../views/home";
+import FullInfo from "../views/full-info";
+import Detail from "../views/detail";
 
 export const MainRoutes = () => (
   <Switch>
-    <Route path="/" component={() => <div>hello world!!</div>} />
+    <Route exact path="/" component={Home} />
+    <Route exact path="/full-info" component={FullInfo} />
+    <Route exact path="/detail/:id" component={Detail} />
   </Switch>
 );
