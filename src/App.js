eb0a765b5917f643bc9c689c88ee654957a3ea41
Yichuan Lin Lee <yichuan.lin.sngular@xbyorange.com>
2019-11-13 16:47:18 +0100

    Add header component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import MainRoutes from "./routes";
+import Header from "./components/header";
 import "./App.css";
 
 function App() {
+  const links = [
+    { url: "/", label: "Home" },
+    { url: "/full-info", label: "Full Info" },
+    { url: "/detail/null", label: "Detail" }
+  ];
   return (
-    <div className="App">
-      <h1>hello world!!</h1>
-      <Router>
-        <MainRoutes />
-      </Router>
-    </div>
+    <Router>
+      <div className="App">
+        <Header links={links} />
+        <div className="main-router-wrapper">
+          <MainRoutes />
+        </div>
+      </div>
+    </Router>
   );
 }
 
