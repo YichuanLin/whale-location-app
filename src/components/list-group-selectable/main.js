bb2b9d53ad2f9eed07d5e56dc0e2eb253b9091b5
Yichuan Lin Lee <yichuan.lin.sngular@xbyorange.com>
2019-11-19 10:45:18 +0100

    Refresh selectable list state from props

--- src/components/list-group-selectable/main.js
+++ src/components/list-group-selectable/main.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./main.css";
 
 export const ListGroupSelectable = ({ list, selectedItem, onSelectedItem }) => {
+  const [hasClicked, setHasClicked] = useState(false);
+  const refs = list.reduce((acc, value) => {
+    acc[value.id] = React.createRef();
+    return acc;
+  }, {});
+
+  useEffect(() => {
+    if (selectedItem && !hasClicked) {
+      refs[selectedItem].current.scrollIntoView({
+        behavior: "auto"
+      });
+    }
+    setHasClicked(false);
+  }, [selectedItem, hasClicked, refs]);
+
+  const onClick = item => {
+    onSelectedItem(item);
+    setHasClicked(true);
+  };
+
   const getClassName = item => (selectedItem === item.id ? "active" : "");
+
   return (
     <ul className="list-group-selectable">
       {list.map((item, index) => (
         <li
+          ref={refs[item.id]}
           key={index}
-          onClick={() => onSelectedItem(item)}
+          onClick={() => onClick(item)}
           className={getClassName(item)}
         >
           {item.id}
