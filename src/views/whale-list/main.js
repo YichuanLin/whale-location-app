c0da39a0795d39534e8e267dee3b5692c8171426
Yichuan Lin Lee <yichuan.lin.sngular@xbyorange.com>
2019-11-21 15:46:05 +0100

    Use useFetch custom hook to fetch data on whale-list

--- src/views/whale-list/main.js
+++ src/views/whale-list/main.js
@@ -1,47 +1,68 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Loader from "../../components/loader";
 import Note from "../../components/note";
-import WithList from "../../components/with-list";
+import ListGroupSelectable from "../../components/list-group-selectable";
+import { useFetch } from "../../hooks";
 
 import "./main.css";
-import { ListGroupSelectable } from "../../components/list-group-selectable/main";
+
+const BASE_URL = "http://hotline.whalemuseum.org/api.json";
+
+const getInitialFetchData = spices => {
+  const url = spices ? `${BASE_URL}?species=${spices}` : BASE_URL;
+  const config = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  };
+  return {
+    url,
+    config
+  };
+};
 
 export const WhaleList = ({ spices, selectedItem, onSelectedItem }) => {
+  const [{ data: list, error, loading }, { setUrl }] = useFetch(
+    getInitialFetchData(spices)
+  );
+
+  useEffect(() => {
+    const url = spices ? `${BASE_URL}?species=${spices}` : BASE_URL;
+    setUrl(url);
+  }, [spices]);
+
+  if (loading) {
+    return (
+      <div className="whale-list__loader-wrapper">
+        <Loader />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <Note type={Note.TYPE.DANGER} title="Error" description={error} />;
+  }
+
+  if (!list) {
+    return null;
+  }
+
+  if (!list.length) {
+    return <Note type={Note.TYPE.INFO} description="Empty results" />;
+  }
+
+  const onChange = ({ id }) => {
+    onSelectedItem(id);
+  };
+
   return (
-    <WithList spices={spices}>
-      {(list, isFetching, error) => {
-        if (isFetching) {
-          return (
-            <div className="whale-list__loader-wrapper">
-              <Loader />
-            </div>
-          );
-        }
-
-        if (error) {
-          return (
-            <Note type={Note.TYPE.DANGER} title="Error" description={error} />
-          );
-        }
-
-        if (!list.length) {
-          return <Note type={Note.TYPE.INFO} description="Empty results" />;
-        }
-
-        const onChange = ({ id }) => {
-          onSelectedItem(id);
-        };
-
-        return (
-          <div className="whale-list__list-wrapper">
-            <ListGroupSelectable
-              list={list}
-              selectedItem={selectedItem}
-              onSelectedItem={onChange}
-            />
-          </div>
-        );
-      }}
-    </WithList>
+    <div className="whale-list__list-wrapper">
+      <ListGroupSelectable
+        list={list}
+        selectedItem={selectedItem}
+        onSelectedItem={onChange}
+      />
+    </div>
   );
 };
