37944928dc5515eeb5ff2b4ff709aea0958b3b2f
Yichuan Lin Lee <yichuan.lin.sngular@xbyorange.com>
2019-11-29 14:15:34 +0100

    Configure store

--- src/App.js
+++ src/App.js
@@ -2,24 +2,35 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import MainRoutes from "./routes";
 import Header from "./components/header";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "./store/reducers";
 
 import "./App.css";
+
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 function App() {
   const links = [
     { url: "/", label: "Home" },
     { url: "/full-info", label: "Full Info" },
-    { url: "/detail/null", label: "Detail" }
+    { url: "/detail/null", label: "Detail" },
+    { url: "/test", label: "Test" }
   ];
   return (
-    <Router>
-      <div className="App">
-        <Header links={links} />
-        <div className="main-router-wrapper">
-          <MainRoutes />
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Header links={links} />
+          <div className="main-router-wrapper">
+            <MainRoutes />
+          </div>
         </div>
-      </div>
-    </Router>
+      </Router>
+    </Provider>
   );
 }
 
